bb899fc3ceca82669d876a667515b49bb623b151
Douglas <douglascruznuness@hotmail.com>
2021-06-21 20:08:45 -0300

    Nlw - Aula 1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button"
+
+function App() {
+  return (
+    <div>
+      <Button text="Button 1" />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
